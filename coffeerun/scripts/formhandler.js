7c5e8a1039c1c7d681aa4548ba611ee507b0b790
Mabel Be <mbe@uwaterloo.ca>
2018-08-13 16:04:23 -0400

    silver challenge

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -1,44 +1,83 @@
 (function(window) {
-    'use strict';
-    var App = window.App || {};
-    var $ = window.jQuery;
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+  var SLIDER_VALUE_SELECTOR = '[data-form-role="value"]';
+  var SLIDER_LOW_CLASS = 'lowValue';
+  var SLIDER_MID_CLASS = 'midValue';
+  var SLIDER_HIG_CLASS = 'higValue';
 
-    function FormHandler(selector) { // Code will go here
-      if (!selector) {
-        throw new Error('No selector provided');
-      }
 
-      //Declare an instance variable named $formElement,$ is just a indicator of using jquery
-      //it returns a single object, and the object contains references to the selected elements
-      this.$formElement = $(selector);
-      if (this.$formElement.length === 0) {
-        throw new Error('Could not find element with selector: ' + selector);
-      }
+
+  function FormHandler(selector) { // Code will go here
+    if (!selector) {
+      throw new Error('No selector provided');
     }
 
-    FormHandler.prototype.addSubmitHandler = function(fn) {
-      console.log('Setting submit handler for form');
+    //Declare an instance variable named $formElement,$ is just a indicator of using jquery
+    //it returns a single object, and the object contains references to the selected elements
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
 
-      this.$formElement.on('submit', function(event) {
-          event.preventDefault();
+  FormHandler.prototype.addSubmitHandler = function(fn) {
+    console.log('Setting submit handler for form');
 
-          // jQuery provides a convenience method (serializeArray) for getting the values from the form. In order to use serializeArray, you need to “wrap” the form using jQuery. Calling $(this) gives you a wrapped object, which has access to the serializeArray method.
-          //serializeArray returns the form data as an array of objects.
-          var data = {};
-          $(this).serializeArray().forEach(function(item) {
-            data[item.name] = item.value;
-            console.log(item.name + ' is ' + item.value);
-          });
+    this.$formElement.on('submit', function(event) {
+      event.preventDefault();
 
-          console.log(data);
-          fn(data);
-          this.reset();
-          //elements is the array of the form fields
-          this.elements[0].focus();
+      // jQuery provides a convenience method (serializeArray) for getting the values from the form. In order to use serializeArray, you need to “wrap” the form using jQuery. Calling $(this) gives you a wrapped object, which has access to the serializeArray method.
+      //serializeArray returns the form data as an array of objects.
+      var data = {};
+      $(this).serializeArray().forEach(function(item) {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
       });
 
+      console.log(data);
+      fn(data);
+      this.reset();
+      //elements is the array of the form fields
+      this.elements[0].focus();
+    });
 
   };
 
-  App.FormHandler = FormHandler; window.App = App;
+  FormHandler.prototype.addSliderHandler=function() {
+    console.log('Setting slider handler for form');
+
+    this.$formElement.on('change', function() {
+      var sliderValue = document.querySelector(SLIDER_VALUE_SELECTOR);
+      sliderValue.innerHTML=this.value;
+      sliderValue.value=this.value;
+      console.log('value is now '+this.value);
+
+      if(sliderValue.value<30){
+        sliderValue.classList.remove(SLIDER_HIG_CLASS);
+        sliderValue.classList.remove(SLIDER_MID_CLASS);
+
+        sliderValue.classList.add(SLIDER_LOW_CLASS);
+
+      }else if(sliderValue.value>=30 && sliderValue.value<60){
+        sliderValue.classList.remove(SLIDER_LOW_CLASS);
+        sliderValue.classList.remove(SLIDER_HIG_CLASS);
+
+        sliderValue.classList.add(SLIDER_MID_CLASS);
+
+      }else{
+        sliderValue.classList.remove(SLIDER_LOW_CLASS);
+        sliderValue.classList.remove(SLIDER_MID_CLASS);
+
+        sliderValue.classList.add(SLIDER_HIG_CLASS);
+
+      }
+      //  if(){
+      //}
+    });
+  };
+
+  App.FormHandler = FormHandler;
+  window.App = App;
 })(window);
--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -2,6 +2,7 @@
 (function (window) {
   'use strict';
   var FORM_SELECTOR='[data-coffee-order="form"]';
+  var SLIDER_SELECTOR='[data-form-role="slider"]'
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
@@ -9,8 +10,10 @@
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
   var formHandler= new FormHandler(FORM_SELECTOR);
+  var sliderHandler=new FormHandler(SLIDER_SELECTOR);
 
   //you cannot just pass a reference to createOrder to formHandler.addSubmitHandler. This is because createOrder’s owner changes when it is invoked inside of the event handling callback. With a different owner, the value of this inside the body of createOrder will not be the Truck instance
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  sliderHandler.addSliderHandler();
   console.log(formHandler);
 })(window);
